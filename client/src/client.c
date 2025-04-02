73fa783b4836ed8855c75e9ff4aebc790d19edfd
agmblanco <agblanco@frba.utn.edu.ar>
2025-04-02 18:05:29 +0000

    Loggin y config

--- client/src/client.c
+++ client/src/client.c
@@ -1,7 +1,8 @@
 #include "client.h"
 
-int main(void)
-{
+t_log* logger;
+
+int main(void){
 	/*---------------------------------------------------PARTE 2-------------------------------------------------------------*/
 
 	int conexion;
@@ -9,7 +10,6 @@ int main(void)
 	char* puerto;
 	char* valor;
 
-	t_log* logger;
 	t_config* config;
 
 	/* ---------------- LOGGING ---------------- */
@@ -18,11 +18,14 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
+	log_info(logger, "Hola! Soy un Log");
 
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
+	
+	log_info(logger, "La config CLAVE tiene como valor: [%s]", config_get_string_value(config, "CLAVE"));
 
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
@@ -52,16 +55,22 @@ int main(void)
 	// Proximamente
 }
 
-t_log* iniciar_logger(void)
-{
-	t_log* nuevo_logger;
+t_log* iniciar_logger(void){
+	t_log* nuevo_logger = log_create("tp0.log", "TP0", true, LOG_LEVEL_INFO);
+
 
 	return nuevo_logger;
 }
 
-t_config* iniciar_config(void)
-{
-	t_config* nuevo_config;
+t_config* iniciar_config(void){
+	char * config_file = "cliente.config";
+
+	t_config* nuevo_config = config_create(config_file);
+
+	if(nuevo_config == NULL){
+		log_error(logger, "Error obteniendo las configuraciones de %s", config_file);
+		exit(1);
+	}
 
 	return nuevo_config;
 }
@@ -95,6 +104,8 @@ void paquete(int conexion)
 
 void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
+	log_destroy(logger);
+	config_destroy(config);
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
 }
